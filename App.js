b0db4ef71457c7a9afc5305e8a169a2838db12c8
Samik Wangneo <59485585+samikwangneo@users.noreply.github.com>
2024-04-20 01:33:55 -0400

    image change

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-          <Text style={styles.nameText}>Change on Eswar Mac</Text>
+          <Text style={styles.nameText}>Name</Text>
+          <Image source={require('./assets/logo.png')} style={styles.image} />
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +21,15 @@ const styles = StyleSheet.create({
     },
     nameText: {
         fontSize: 24,
+        alignItems: 'center',
+        position: 'absolute',
+        top: '15%',
+    },
+    image: {
+        width: 150,
+        height: 140,
+        alignItems: 'center',
+        position: 'absolute',
+        top: '25%',
     },
 });
