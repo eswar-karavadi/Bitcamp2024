88fa0c3e17385d326f1000d4a4649e0e324bb566
Samik Wangneo <59485585+samikwangneo@users.noreply.github.com>
2024-04-19 23:35:14 -0400

    test commit

--- App.js
+++ App.js
@@ -4,17 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+          <Text style={styles.nameText}>Name</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    nameText: {
+        fontSize: 24,
+    },
 });
