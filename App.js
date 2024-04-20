afcc5def59d151cb52ea91ce010eb6c923086263
eswar-karavadi <125041114+eswar-karavadi@users.noreply.github.com>
2024-04-19 23:48:39 -0400

    Change from mac

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-          <Text style={styles.nameText}>Name Hello World HELLOOOOOOO</Text>
+          <Text style={styles.nameText}>Change on Eswar Mac</Text>
       <StatusBar style="auto" />
     </View>
   );
